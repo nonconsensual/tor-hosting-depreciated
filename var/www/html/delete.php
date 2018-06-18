8819ddb360e4473c31b88694bda67630bb103cf8
RWL <rwl@Mac.local>
2018-06-18 12:17:25 +0100

    theme change
    updated home.php delete.php password.php theme mod

--- var/www/html/delete.php
+++ var/www/html/delete.php
@@ -1,5 +1,37 @@
 <?php
 include('../common.php');
+//included new theme start
+echo "<link rel=\"stylesheet\" href=\"/css/st.css\">\n";
+echo "<link rel=\"stylesheet\" href=\"/css/tb.css\">\n";
+echo "<link rel=\"stylesheet\" href=\"/css/font-awesome/css/font-awesome.min.css\">\n";
+echo "\n";
+echo "<body class=\"w3-theme-bd\">\n";
+//echo "<div class=\"w3-top w3-bar w3-dark-grey\">\n";
+echo "<div class=\"w3-top w3-bar w3-teal\">\n";
+//echo "<div class=\"w3-top w3-bar w3-blue\">\n";
+//include('../common.php');
+
+echo "<a class=\"w3-hide-medium w3-hide-large w3-bar-item w3-button w3-theme-hm\" href=\"/\" title=\"WowMee Hosting\"><i class=\"fa fa-home\"></i></a>\n";
+echo "<a href=\"/\" class=\"w3-hide-small w3-bar-item w3-button w3-theme-hm\" title=\"WowMee Hosting\"><i class=\"fa fa-home w3-margin-right\"></i>WowMee Hosting</a>\n";
+echo "<a href=\"/register.php\" class=\"w3-bar-item w3-button\" title=\"Register\">Register</a>\n";
+echo "<a href=\"/login.php\" class=\"w3-bar-item w3-button\" title=\"Login\">Login</a>\n";
+echo "<a href=\"/list.php\" class=\"w3-hide-small w3-bar-item w3-button\" title=\"List of hosted sites\">List</a>\n";
+echo "<a class=\"w3-bar-item w3-hide-medium w3-hide-large w3-button\" href=\"/list/\" title=\"List of hosted sites\"><i class=\"fa fa-sticky-note\"></i></a>\n";
+echo "<a href=\"/faq.php\" class=\"w3-hide-small w3-bar-item w3-button\" title=\"FAQ\">FAQ</a>\n";
+echo "<a class=\"w3-bar-item w3-hide-medium w3-hide-large w3-button w3-gre\" href=\"/faq\" title=\"FAQ\"><i class=\"fa fa-book\"></i></a>\n";
+echo "<a href=\"/contact.php\" class=\"w3-bar-item w3-button w3-hide-small\" title=\"Contact me\">Contact</a>\n";
+echo "<a class=\"w3-bar-item w3-button w3-hide-medium w3-hide-large\" href=\"/contact/\" title=\"Contact me\"><i class=\"fa fa-envelope\"></i></a>\n";
+echo "</div>\n";
+echo "<div class=\"w3-row-padding w3-padding-64\">\n";
+echo "<div class=\"w3-container\">\n";
+//echo "<header class=\"w3-container w3-dark-grey\">\n";
+echo "<header class=\"w3-container w3-black\">\n";
+echo "<h2 class=\"w3-center\">WowMee Hosting</h2>\n";
+echo "</header>\n";
+echo "<div class=\"w3-container w3-grey\">\n";
+echo "<p class=\"w3-left w3-tag w3-small\">All Systems Active..</p>\n";
+echo "<div class=\"w3-display-container w3-container\">\n";
+//include new theme end
 try{
 	$db=new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS, [PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING, PDO::ATTR_PERSISTENT=>PERSISTENT]);
 }catch(PDOException $e){
